f145e432a3c099c2bfdad30e904df9c12239d0c4
supcanc <mainmailforwork@gmail.com>
2025-11-27 16:45:41 +0500

    Fixed docker

--- Dockerfile
+++ Dockerfile
@@ -4,8 +4,10 @@ WORKDIR /workdir
 
 COPY . .
 
+RUN pip install torch==2.9.1+cu130 torchvision==0.24.1+cu130 --index-url https://download.pytorch.org/whl/cu130
+
 RUN pip install -r requirements.txt
 
 EXPOSE 80
 
-CMD [ "python", "server/app.py" ]
+CMD [ "python", "api/app.py" ]
--- api/app.py
+++ api/app.py
@@ -1,17 +1,25 @@
 import torch
 from fastapi import FastAPI, HTTPException
-from torchvision import models
-import config
+from torchvision import models, transforms
 import os
 from PIL import Image
 import uvicorn
 
 app = FastAPI()
 
+MODEL_PATH = 'models/best_model.pth'
+CLASSES_NAMES = ['Angry', 'Fear', 'Happy', 'Sad', 'Suprise']
+EVAL_TRANSFORMS = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.CenterCrop((224, 224)),
+    transforms.ToTensor(),
+    transforms.Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
+])
+
 device = torch.accelerator.current_accelerator() if torch.accelerator.is_available() else 'cpu'
 model = models.resnet18(weights=None)
-model.fc = torch.nn.Linear(model.fc.in_features, len(config.CLASSES_NAMES))
-model.load_state_dict(torch.load(config.MODEL_PATH, weights_only=True))
+model.fc = torch.nn.Linear(model.fc.in_features, len(CLASSES_NAMES))
+model.load_state_dict(torch.load(MODEL_PATH, weights_only=True, map_location=device))
 model.to(device)
 model.eval()
 
@@ -19,13 +27,13 @@ model.eval()
 def predict(images_path:str):
     images = []
     images_names = []
-    classes_names = config.CLASSES_NAMES
+    classes_names = CLASSES_NAMES
 
     try:
         for image_name in os.listdir(images_path):
             image_path = os.path.join(images_path, image_name)
             image = Image.open(image_path).convert('RGB')
-            transformations = config.EVAL_TRANSFORMS
+            transformations = EVAL_TRANSFORMS
             image = transformations(image).cpu().detach().tolist()
             images_names.append(image_name)
             images.append(image)
@@ -34,7 +42,7 @@ def predict(images_path:str):
         probabilities = torch.softmax(model(images), dim=1).cpu().detach()
         predicted_classes = torch.argmax(probabilities, dim=1).cpu().detach().tolist()
 
-        with open('../predictions/predictions.txt', 'w') as f:
+        with open('predictions/predictions.txt', 'w') as f:
             for i in range(len(predicted_classes)):
                 f.write(f'{i + 1}) Filename: {images_names[i]}\n'
                         f'Class: {classes_names[predicted_classes[i]]}\n'
